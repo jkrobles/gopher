03871fd842b50617d99b326d90b903344159a0ca
Justice <justice@justicerobles.com>
2016-05-28 05:32:06 -0400

    Add ODP function

--- gopher.go
+++ gopher.go
@@ -13,3 +13,9 @@ func Slg(TB float32, AB float32 ) (slugging float32) {
   slugging = (TB/AB)
   return slugging
 }
+
+func Odp(H float32, BB float32, HBP float32, AB float32, SF float32) (onbase float32) {
+    onbase = ((H+BB+HBP)/(AB+BB+SF+HBP))
+    return
+}
+
